1e1791d5cb7e9fa3c2fefec92f7ad0adba8d37e2
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-05 22:05:51 +0500

    all colors is in this file

---
+++ lib/res/my_colors/my_colors.dart
@@ -0,0 +1,14 @@
+
+
+
+
+import 'dart:ui';
+
+class MyColors {
+
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const lightBlue = Color(0xFF00a3ff);
+  static const darkGray = Color(0xFF696969);
+  static const lightGray = Color(0xFFf4f4f4);
+
+}
